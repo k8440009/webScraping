544f4868b3d9b29fc387d9f4f73ce267797147e7
lucas0730 <lucas0730@naver.com>
2019-12-02 14:29:38 +0900

    Extracting Locations and Finishing up

--- indeed.py
+++ indeed.py
@@ -24,23 +24,34 @@ def extract_indeed_pages():
     return max_page
 
 
+def extract_job(html):
+    title = html.find("div", {"class": "title"}).find("a")["title"]   # 제목 추출
+    company = html.find("span", {"class": "company"})
+    company_anchor = company.find("a")
+    # 회사 링크가 없는 경우
+    if company_anchor is not None:
+        company = str(company_anchor.string)
+    # 링크가 있는 경우
+    else:
+        company = str(company.string)
+    company = company.strip()
+    location = html.find("div", {"class": "recJobLoc"})["data-rc-loc"]
+    job_id = html["data-jk"]
+    return {'title': title,
+            'company': company,
+            'location': location,
+            'link': f"https://www.indeed.com/viewjob?jk={job_id}"}
+
+
 def extract_indeed_jobs(last_page):
     jobs = []
-    result = requests.get(f"{URL}&start={0*LIMIT}")
-    soup = BeautifulSoup(result.text, "html.parser")
-    results = soup.find_all(
-        "div", {"class": "jobsearch-SerpJobCard"})  # 일자리 추출
-    for result in results:
-        title = result.find("div", {"class": "title"}).find("a")[
-            "title"]   # 제목 추출
-        company = result.find("span", {"class": "company"})
-        company_anchor = company.find("a")
-        # 회사 링크가 없는 경우
-        if company_anchor is not None:
-            company = str(company_anchor.string)
-        # 링크가 있는 경우
-        else:
-            company = str(company.string)
-        company = company.strip()
-        print(company)
+    for page in range(last_page):
+        print(f"Scrapping page {page}")
+        result = requests.get(f"{URL}&start={page*LIMIT}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all(
+            "div", {"class": "jobsearch-SerpJobCard"})  # 일자리 추출
+        for result in results:
+            job = extract_job(result)
+            jobs.append(job)
     return jobs
